907a94dbf87c5e2baa2b4773ebd3f768dccbd60f
João Victor <jvssmello@gmail.com>
2023-01-04 21:55:43 -0400

    img

--- express-app/app.js
+++ express-app/app.js
@@ -31,14 +31,14 @@ const DC = require('./routes/debitoCredito/DC');
 
 
 //view engine setup
-app.set("views", path.join(__dirname, "/views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", routes);
 app.use('/', investidorRouter);
