32d8332ddf3ea6832c3c7d480755522f32455b9d
João Victor <98440760+JooMello@users.noreply.github.com>
2024-06-21 16:12:23 -0400

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const connection = new Sequelize("farm", "root", "T5u9w3p6#", {
-  host: "104.154.150.110",
+  host: "35.239.71.4",
   port: "3306",
   dialect: "mysql",
   timezone: "-03:00",
